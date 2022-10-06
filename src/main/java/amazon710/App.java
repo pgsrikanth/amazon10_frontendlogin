b72087e2bc7cd02cfc7471df8f58e458241babb0
srikanth <gowtham@ssc.com>
2022-10-07 03:10:37 +0530

    maven project

---
+++ src/main/java/amazon710/App.java
@@ -0,0 +1,13 @@
+package amazon710;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
